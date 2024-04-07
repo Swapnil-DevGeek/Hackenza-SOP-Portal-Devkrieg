4f70ef0168c468430e765afe82121e251deab757
darthlazius <malviyakushagra17@gmail.com>
2024-04-07 07:04:21 +0530

    mera ho gaya

--- server/index.js
+++ server/index.js
@@ -84,6 +84,83 @@ db.run('insert into student_project_applications (student_id, project_id, gpsrn,
 })
 })
 
+app.get('/api/v1/admin',(req,res)=>{
+  
+  const header = req.headers.authorization;
+  if(!header){
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  
+  const token = header;
+  jwt.verify(token, 'your_secret_key', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    } else {
+      console.log(decoded);
+      const {userId} = decoded;
+
+      const sql = `SELECT * FROM sop_dop_sat_projects`;
+
+      // Execute the query
+      db.all(sql, (err, rows) => {
+        if (err) {
+          console.error(err.message);
+          res.status(500).send('Internal Server Error');
+        } else {
+          res.json(rows); // Send the fetched projects as JSON response
+        }
+      });
+    }
+  });
+});
+
+
+
+app.get('/api/v1/faculty',(req,res)=>{
+  const header = req.headers.authorization;
+  if(!header){
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  
+  const token = header;
+  jwt.verify(token, 'your_secret_key', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }else{
+      console.log(decoded);
+    }
+    const {userId} = decoded;
+
+    const sql = `SELECT * FROM sop_dop_sat_projects WHERE faculty_id = ?`;
+
+  // Execute the query
+  db.all(sql, [userId], (err, rows) => {
+      if (err) {
+          console.error(err.message);
+          res.status(500).send('Internal Server Error');
+      } else {
+          res.json(rows); // Send the fetched applications as JSON response
+      }
+  });
+})
+})
+
+
+
+app.post('/api/v1/faculty-projects', (req, res) => {
+  const{project_id,gpsrn,project_title,project_description,date,research_areas,status,comments,faculty_id} = req.body;
+
+  db.run('insert into sop_dop_sat_projects (project_id,gpsrn,project_title,project_description,date,research_areas,status,comments,faculty_id) values (?,?,?,?,?,?,?,?,?)',
+  [project_id,gpsrn,project_title,project_description,date,research_areas,status,comments,faculty_id], function(err){
+    if(err){
+      console.error(err);
+      return res.status(500).json({error: 'Internal server error'});
+    }
+    res.json({message : 'Project advertised successfully', project_id : this.lastID});
+  })
+
+})
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
