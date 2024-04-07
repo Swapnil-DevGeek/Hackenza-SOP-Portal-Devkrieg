717def1326134d30dbff942c4313122121fab133
darthlazius <malviyakushagra17@gmail.com>
2024-04-07 06:28:50 +0530

    new routes for student

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 const jwt = require('jsonwebtoken');
-
+const allapplicationRoute = require('./routes/student-routes/allapplication');
 const app = express();
 const port = 3000;
 
@@ -31,8 +31,59 @@ app.post('/api/v1/signin', (req, res) => {
     const token = jwt.sign({ userId: row.id, role : row.role }, 'your_secret_key');
     
     res.json({ message: 'Login successful!', token });
+
   });
 });
+
+//routes 
+// app.use('/api/v1/application',allapplicationRoute);
+
+
+app.get('/api/v1/application-all', (req, res) => {
+  // Check if user is authenticated
+  
+  const header = req.headers.authorization;
+  if(!header){
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  
+  const token = header;
+  jwt.verify(token, 'your_secret_key', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }else{
+      console.log(decoded);
+    }
+    const {userId} = decoded;
+
+    const sql = `SELECT * FROM student_project_applications WHERE student_id = ?`;
+
+  // Execute the query
+  db.all(sql, [userId], (err, rows) => {
+      if (err) {
+          console.error(err.message);
+          res.status(500).send('Internal Server Error');
+      } else {
+          res.json(rows); // Send the fetched applications as JSON response
+      }
+  });
+})
+});
+
+app.post('/api/v1/application', (req, res) => {
+
+const{student_id, project_id, gpsrn,comments,justification,status,application_date,faculty_comment} = req.body;
+
+db.run('insert into student_project_applications (student_id, project_id, gpsrn, comments, justification, status, application_date, faculty_comment) values (?,?,?,?,?,?,?,?)',
+[student_id, project_id, gpsrn, comments, justification, status, application_date, faculty_comment], function(err){
+  if(err){
+    console.error(err);
+    return res.status(500).json({error: 'Internal server error'});
+  }
+  res.json({message : 'Application submitted successfully', application_id : this.lastID});
+})
+})
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
